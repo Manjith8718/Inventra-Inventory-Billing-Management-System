e86aa29069d57c845f0000512cc72ca0090527e8
ManjithKanukolanu <manjithkanukolanu87@gmail.com>
2026-02-11 12:58:25 +0530

    Owner Model Created

---
+++ src/Models/Owner.java
@@ -0,0 +1,37 @@
+package Models;
+public class Owner {
+     private  int owner_id;
+     private String email;
+     private String password;
+
+     public Owner(){}
+     public Owner(int owner_id,String email, String password) {
+         this.owner_id = owner_id;
+         this.email = email;
+         this.password = password;
+     }
+
+    public int getOwner_id() {
+        return owner_id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setOwnerId(int owner_id) {
+        this.owner_id = owner_id;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
