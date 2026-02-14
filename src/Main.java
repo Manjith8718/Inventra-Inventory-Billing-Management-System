48060779e507517ca2c395e0b073e3e887f95728
ManjithKanukolanu <manjithkanukolanu87@gmail.com>
2026-02-14 16:02:36 +0530

    Updated Main Logic

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,35 @@
+import Services.CustomerService;
+import Services.OwnerService;
+import java.util.Scanner;
 public class Main
 {
     public  static void main(String[] args)
     {
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            System.out.println("\n==== INVENTRA SYSTEM ====");
+            System.out.println("1. Owner");
+            System.out.println("2. Customer");
+            System.out.println("3. Exit");
+            System.out.print("Choose option: ");
 
+            int choice = sc.nextInt();
+            sc.nextLine();
+            switch (choice) {
+                case 1:
+                    OwnerService.start();
+                    break;
+                case 2:
+                    CustomerService.start();
+                    break;
+                case 3:
+                    System.out.println("Exiting system...");
+                    System.exit(0);
+                default:
+                    System.out.println("Invalid choice. Try again.");
+            }
+        }
     }
 }
+
+
